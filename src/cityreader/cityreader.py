1078b0f04a8b6767497ac7f08622ade140324e6e
theolamide <ajadidan@gmail.com>
2020-03-18 13:50:58 -0600

    MVP Complete

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -9,6 +9,9 @@ class City:
         self.lat = lat
         self.lon = lon
 
+    def __repr__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
